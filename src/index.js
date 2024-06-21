00b7f4f612602abcb30b40975240997997032d37
NguyenNhuDuyHung <duyhung03112004@gmail.com>
2024-06-21 13:32:17 +0700

    [MVC] Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -5,22 +5,27 @@ const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
 // Static file
 app.use(express.static(path.join(__dirname, "public")));
 
+// Middleware
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+
 // Http logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // Template Engine
-app.engine("hbs", hbs.engine({extname: '.hbs'}));
+app.engine("hbs", hbs.engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-console.log(__dirname);
+// Routes
+// res: response from server to client
+// req: request from client to server
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
